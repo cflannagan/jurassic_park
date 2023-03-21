80a909af2bacd88c9305837cb970a1f783b292da
Craig Flannagan <cflannagan@gmail.com>
2023-03-21 12:16:21 -0400

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# SPECIES
+
+# carnivores
+%i[Tyrannosaurus Velociraptor Spinosaurus Megalosaurus].each do |carnivore|
+  Species.find_or_create_by!(name: carnivore, dinosaur_type: "carnivore")
+  puts "Seeded #{carnivore} species"
+end
+
+# herbivores
+%i[Brachiosaurus Stegosaurus Ankylosaurus Triceratops].each do |herbivore|
+  Species.find_or_create_by!(name: herbivore, dinosaur_type: "herbivore")
+  puts "Seeded #{herbivore} species"
+end
+
+# CAGES
+[2, 3, 4, 5].each do |capacity|
+  Cage.find_or_create_by!(capacity:)
+  puts "Cage with capacity of #{capacity} added to Jurassic Park!"
+end
+
+# DINOSAURS
+dinosaurs = [
+  {"Ty" => :Tyrannosaurus},
+  {"Roberta" => :Tyrannosaurus},
+
+  # Blue's Pack
+  {"Blue" => :Velociraptor},
+  {"Charlie" => :Velociraptor},
+  {"Delta" => :Velociraptor},
+  {"Echo" => :Velociraptor},
+
+  {"Spiny" => :Spinosaurus},
+  {"Mega" => :Megalosaurus},
+  {"Brachy" => :Brachiosaurus},
+  {"Rocky" => :Stegosaurus},
+  {"Tank" => :Ankylosaurus},
+  {"Trixie" => :Triceratops},
+]
+dinosaurs.each do |dinosaur|
+  name, species_name = dinosaur.first
+  species = Species.find_by(name: species_name)
+  Dinosaur.find_or_create_by!(name:, species:)
+  puts "#{name}, a #{species_name}, has been added to Jurassic Park!"
+end
